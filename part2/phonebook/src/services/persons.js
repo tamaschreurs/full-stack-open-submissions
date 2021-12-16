66ce08ecc3755e1607c16d5241999f085a29e335
Tama <tama.schreurs@gmail.com>
2021-12-16 01:27:24 +0100

    3.9-3.11 ready for internet deployment

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "/api/persons";
 
 const addPerson = (person) => {
   const post = axios.post(baseUrl, person);
