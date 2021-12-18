1ce7ca77fedb9c3813cbe4dc9f7a4db37638373c
Tama <tama.schreurs@gmail.com>
2021-12-19 00:35:16 +0100

    4.2 config and logger

--- part4/index.js
+++ part4/index.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
 const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -14,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
@@ -34,7 +35,6 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
