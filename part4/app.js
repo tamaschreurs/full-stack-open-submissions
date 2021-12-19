b17bfff4e4f0a440b72c1ea54f2b77268b1aa7b2
Tama <tama.schreurs@gmail.com>
2021-12-19 01:15:34 +0100

    blogrouter and blogschema separated

--- part4/app.js
+++ part4/app.js
@@ -3,36 +3,15 @@ const app = express();
 
 const cors = require("cors");
 const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blogs");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
+app.use("/api/blogs", blogRouter);
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 
 module.exports = app;
