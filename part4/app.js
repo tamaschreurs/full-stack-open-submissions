b65c41fe7e9006fbb79e40055881fafdcf4c4746
Tama <tama.schreurs@gmail.com>
2022-01-16 21:42:11 +0100

    4.18 token authentication

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRouter = require("./controllers/users");
 const blogRouter = require("./controllers/blogs");
+const loginRouter = require("./controllers/login");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
@@ -21,6 +22,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
 
