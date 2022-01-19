96259cabf2103d33ce5573c7151064c7f744f8d7
Tama <tama.schreurs@gmail.com>
2022-01-19 15:48:48 +0100

    5.7

--- part5/bloglist-frontend-main/src/components/Blog.js
+++ part5/bloglist-frontend-main/src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expandedView, setExpandedView] = useState(false);
+  const borderStyle = { border: "solid", padding: "5px" };
+
+  console.log(blog);
+
+  let content;
+  if (!expandedView) {
+    content = (
+      <p style={borderStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setExpandedView(true)}>view</button>
+      </p>
+    );
+  } else {
+    content = (
+      <p style={borderStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={() => setExpandedView(false)}>hide</button>
+        <br />
+        {blog.url}
+        <br />
+        {blog.likes} <button>like</button>
+        <br />
+        {blog.user ? blog.user.name : ""}
+      </p>
+    );
+  }
+
+  return <div>{content}</div>;
+};
+
+export default Blog;
